303e19331e55bc362645387628a7b05134610653
jovan-cn <jovan.cn@outlook.com>
2024-09-16 15:41:47 +0800

    feat: add icon for product card

--- app/components/product/product.tsx
+++ app/components/product/product.tsx
@@ -1,4 +1,3 @@
-'use client'
 import classNames from "classnames";
 import { TProduct } from "@/app/types";
 
@@ -6,26 +5,20 @@ export interface ProductProps {
   p: TProduct,
 }
 export const Product: React.FC<ProductProps> = ({p}) => {
-  const goto = (href: string) => {
-    if (href !== "") window.open(href)
-  }
-
   return (
-    <div className={classNames(
-      'flex flex-row gap-2 items-center',
-      'rounded min-w-80 h-20 hover:cursor-pointer',
+    <a className={classNames(
+      'flex flex-row gap-2 px-2 items-center',
+      'rounded-lg min-w-80 h-20 hover:cursor-pointer',
       'bg-slate-100 dark:bg-slate-800',
-      'hover:bg-slate-300 hover:dark:bg-slate-700',
+      'hover:bg-slate-200 hover:dark:bg-slate-700',
       'text-slate-900 dark:text-slate-400',
-    )} onClick={() => goto(p.host)}>
-      <div>
-        <img src={p.icon} />
-      </div>
+      'shadow-md'
+    )} href={p.host} target="_blank" rel="noreferrer">
+      {p?.icon && <img className={"w-16"} src={p.icon} />}
       <div>
         <h2 className="font-bold text-base">{p.name}</h2>
         <p className="text-sm">{p.desc}</p>
       </div>
-    </div>
-
+    </a>
   )
 }
